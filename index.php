11de355e4e574090fd4dd473af9f19068cbe4f99
BMF970 <brianmartines119@yahoo.com>
2023-08-25 14:08:50 -0600

    Agruegando tabla html

--- index.php
+++ index.php
@@ -2,8 +2,241 @@
 <html lang="en">
 <head>
     <?php include "/views/templates/head.php" ?>
+    
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
 
+    <section class="container">
+    <form action="/controller/" method="post">
+    <table class="default">
+        <tr>
+            <th>Produccion</th>
+            <th>¿Cuanto hay hecho?</th>
+            <th></th>
+            <th>¿Cuanto se hara?</th>
+            <th>Lo realizara</th>
+        </tr>
+        <tr>
+            <td>Estaciones</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>            
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>            
+        </tr>
+        <tr>
+            <td>Repollo</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>
+        </tr>
+        <tr>
+            <td>Queso</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Zanahoria</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Jicama</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Pepino</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Mango</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Manzanas</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Tamarindos</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Limones</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Jitomate</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Cueritos</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Chetto Flamin' hot</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Uber (Chiles, Chamoy, etc)</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Bajar mayonesas y nutellas</td>
+            <td><input type="text" size="5" min="0" placeholder="0" maxlength="2" required></td>    
+            <td><select name="select" id="sel">
+                <option value="p">Palangana</option>
+                <option value="k">Kilos</option>
+                <option value="b">Botes/cubetas</option>
+                <option value="p">Piezas</option>
+                <option value="i">Inserto</option>
+                <option value="na">No aplica</option>
+            </select></td>
+            <td>texto automatizado</td>
+            <td><input type="text" maxlength="20"></td>    
+        </tr>
+        <tr>
+            <td>Trastes</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td><input type="text"></td>    
+        </tr>   
+    </table>
+    <input type="button" value="enviar">
+    </form>    
+</section>
 </body>
 </html>
--- views/templates/head.php
+++ views/templates/head.php
@@ -4,10 +4,9 @@
     <meta name="keywords" content="app, bmf970, brian martinez flores">
   	<meta name="description" content="App for my family's bussines bmf970">
   	<meta name="author" content="bmf970">
-	<meta name="owner" content="bmf970">
+	  <meta name="owner" content="bmf970">
     <meta name="robots" content="index, follow">
     <link rel="shortcut icon" href="icono">
     <link rel="stylesheet" href="assets/css/reset.css">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet"  href="iconos, etc">
+    <!--<link rel="stylesheet"  href="iconos, etc">-->
     <title>BMF970</title>
